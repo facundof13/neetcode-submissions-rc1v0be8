b0e85ac28e131c02d4c66eb7436c5c72e6109c54
facundof13 <facundof13@gmail.com>
2026-04-01 01:17:56 -0400

    Add: search-for-word - submission-25

---
+++ Data Structures & Algorithms/search-for-word/submission-25.py
@@ -0,0 +1,33 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        S = word[0]
+        global path
+        path = set()
+        def dfs(x, y, idx):
+            if x >= len(board) or x < 0:
+                return False
+            if y >= len(board[0]) or y < 0:
+                return False
+            if idx >= len(word):
+                return False
+            if board[x][y] != word[idx]:
+                return False
+            if (x,y) in path:
+                return False
+            if idx == len(word) - 1:
+                print(idx, word, x, y)
+                return True
+            
+            path.add((x,y))
+            res = dfs(x + 1, y, idx + 1) or dfs(x, y + 1, idx + 1) or dfs(x - 1, y, idx + 1) or dfs(x, y - 1, idx + 1)
+            if res:
+                return True
+            path.remove((x,y))
+
+        for x, row in enumerate(board):
+            for y, char in enumerate(row):
+                if char == S:
+                    matches = dfs(x, y, 0)
+                    if matches:
+                        return True
+        return False
